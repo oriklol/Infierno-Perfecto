4fba77911712682e6faf638110650c5cdcf14638
ciro <marateaciro@gmail.com>
2025-09-23 18:26:06 -0300

    Añadido: Pantalla de gamer over, junto con la musica

---
+++ core/src/main/java/com/dojan/infiernoperfecto/pantallas/PantallaGameOver.java
@@ -0,0 +1,129 @@
+package com.dojan.infiernoperfecto.pantallas;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.dojan.infiernoperfecto.elementos.Imagen;
+import com.dojan.infiernoperfecto.elementos.Musica;
+import com.dojan.infiernoperfecto.elementos.Texto;
+import com.dojan.infiernoperfecto.utiles.Config;
+import com.dojan.infiernoperfecto.utiles.ControlAudio;
+import com.dojan.infiernoperfecto.utiles.Recursos;
+import com.dojan.infiernoperfecto.utiles.Render;
+import io.Entradas;
+
+import static com.dojan.infiernoperfecto.utiles.Render.app;
+
+public class PantallaGameOver implements Screen {
+    private Musica musicaFondo;
+    private Imagen gameOver;
+
+    private Texto opciones[] = new Texto[2];
+    String textos[] = {
+        "PERDISTE!!",
+        "Volver al menu"
+    };
+    private int opc = 2;
+    private float tiempo;
+    private boolean mouseClick = false;
+
+    Entradas entradas = new Entradas();
+
+    @Override
+    public void show() {
+        gameOver = new Imagen(Recursos.FONDOPERDER);
+        musicaFondo = new Musica(Recursos.MUSICAMUERTE);
+        ControlAudio.setMusicaActual(musicaFondo);
+        Gdx.input.setInputProcessor(entradas);
+
+        for(int i = 0; i < opciones.length; i++){
+            opciones[i] = new Texto(Recursos.FUENTEMENU, 90, Color.WHITE, true);
+            opciones[i].setTexto(textos[i]);
+            float anchoTexto = opciones[i].getAncho();
+            int centroX = Config.ANCHO / 3;
+            int x = (int)(centroX - anchoTexto / 2);
+            int y = (int)(Config.ALTO * 0.8f) - (i * 120);
+            opciones[i].setPosition(x, y);
+        }
+
+    }
+
+
+    @Override
+    public void render(float delta) {
+        ControlAudio.reproducirMusica();
+        Render.batch.begin();
+        gameOver.dibujar();
+        for(int i = 0; i<opciones.length;i++){
+            opciones[i].dibujar();
+        }
+        Render.batch.end();
+
+
+
+        tiempo+= delta;
+
+        if(entradas.isAbajo()){
+            if(tiempo>0.15f){
+                tiempo=0;
+                opc++;
+                if(opc>2){
+                    opc=2;
+                }
+            }
+
+        }
+
+        if(entradas.isArriba()){
+            if(tiempo>0.1f){
+                tiempo=0;
+                opc--;
+                if(opc<2){
+                    opc=2;
+                }
+            }
+
+        }
+
+        for(int i=0; i<opciones.length;i++){
+            if(i==(opc-1)){
+                opciones[i].setColor(Color.GOLDENROD);
+            }else{
+                opciones[i].setColor(Color.WHITE);
+            }
+        }
+
+        if(entradas.isEnter() || entradas.isClick()) {
+            if (((opc == 2) && (entradas.isEnter())) || ((opc == 2) && (entradas.isClick()) && (mouseClick))) {
+                ControlAudio.pararMusica();
+                app.setScreen(new PantallaMenu());
+            }
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
